ca9587edef3980c05b4a57ff63f9e19fb18c77db
sss1337xyz <banany4sv@gmail.com>
2022-08-10 00:50:28 +0300

    add products page

--- menu/models/category.py
+++ menu/models/category.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 from menu.models.base import BaseModel
 from menu.models.restauraunt import Restauraunt
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'categorys/{0}/{1}'.format(instance.name, filename)
 
 
 class Category(BaseModel):
     restauraunt = models.ForeignKey(Restauraunt, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, db_index=True, help_text="Введите название категории")
-    photo = models.ImageField(upload_to='categorys/%Y/%m/%d', blank=True)
+    photo = models.ImageField(upload_to=user_directory_path, blank=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
@@ -16,3 +20,6 @@ class Category(BaseModel):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
